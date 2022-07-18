e55f3a222cb112549611c80c8687469e55d60605
muer zhang <muerzhangnz@gmail.com>
2022-07-18 14:17:27 +1200

    Add forecast js & css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import Search from './components/search/search'
 import CurrentWeather from './components/current-weather/current-weather'
+import Forecast from './components/forecast/forecast'
 import { WEATHER_API_URL, WEATHER_API_KEY } from './api'
 import { useState } from 'react'
 
@@ -30,13 +31,13 @@ function App() {
       .catch((err) => console.log(err))
   }
 
-  console.log(currentWeather)
   console.log(forecast)
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   )
 }
