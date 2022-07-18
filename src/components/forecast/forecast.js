707a6544111818e17913f1d5d3e3a509bed334f7
muer zhang <muerzhangnz@gmail.com>
2022-07-18 14:26:30 +1200

    Add accordion item - can display image for 7 days

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,10 +1,33 @@
-import { Accordion } from 'react-accessible-accordion'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemPanel,
+  AccordionItemButton,
+} from 'react-accessible-accordion'
 
 const Forecast = ({ data }) => {
   return (
     <>
       <label className="title">Daily</label>
-      <Accordion allowZeroExpanded></Accordion>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, idx) => (
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    alt="weather"
+                    className="icon-small"
+                    src={`icons/${item.weather[0].icon}.png`}
+                  />
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </>
   )
 }
